d0ade0e1865889c9313c9eb5b06997db16371e6d
sk0145082-maker <sk0145082@gmail.com>
2026-05-10 17:31:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,8 @@ PORT = environ.get("PORT", "8080")
 CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
-CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "https://t.me/dbfilestore")
+CDB_NAME = environ.get("CDB_NAME", "dbfilestore")
 
 # Database Information
 DB_URI = environ.get("DB_URI", "")
